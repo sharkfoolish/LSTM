0001801a5dd7a8a3e0c19542d1b302a65e43b876
seafarer0675 <411077003@mail.nknu.edu.tw>
2024-12-22 14:26:09 +0800

    set empty list to remember loss during initialization

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ def train_test_split(data, target, train_size, test_size, random_state=None):
 
 class CustomLSTM:
     def __init__(self, input_dim, hidden_dim, output_dim, learning_rate=0.01, reg_lambda=0.01, decay_factor=0.01):
+        self.losses = []
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.output_dim = output_dim
